6b7f24266f99257193f4d0f999bb51715700bd7d
Spencer Lott <spklott@gmail.com>
2023-09-11 17:18:55 -0400

    saving

--- PersonalProfile/client/personalprofile/src/App.js
+++ PersonalProfile/client/personalprofile/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-adsfadsfadsfadsfadsfasdf    </div>
+      Welcome to my personal profile page
+</div>
   );
 }
 
